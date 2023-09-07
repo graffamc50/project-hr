cc6ed2ea7b0019189134a6417d2f9693d30ccfa0
Colton Bradley Graffam <colton.b.graffam@gmail.com>
2023-09-07 12:24:31 -0700

    Create IndexNavbar.js

---
+++ src/components/Navbars/IndexNavbar.js
@@ -0,0 +1,138 @@
+
+
+
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+import {
+  Collapse,
+  NavbarBrand,
+  Navbar,
+  NavItem,
+  NavLink,
+  Nav,
+  Container,
+  Row,
+  Col,
+  UncontrolledTooltip,
+} from "reactstrap";
+
+export default function IndexNavbar() {
+  const [collapseOpen, setCollapseOpen] = React.useState(false);
+  const [collapseOut, setCollapseOut] = React.useState("");
+  const [color, setColor] = React.useState("navbar-transparent");
+  React.useEffect(() => {
+    window.addEventListener("scroll", changeColor);
+    return function cleanup() {
+      window.removeEventListener("scroll", changeColor);
+    };
+  }, []);
+  const changeColor = () => {
+    if (
+      document.documentElement.scrollTop > 99 ||
+      document.body.scrollTop > 99
+    ) {
+      setColor("bg-info");
+    } else if (
+      document.documentElement.scrollTop < 100 ||
+      document.body.scrollTop < 100
+    ) {
+      setColor("navbar-transparent");
+    }
+  };
+  const toggleCollapse = () => {
+    document.documentElement.classList.toggle("nav-open");
+    setCollapseOpen(!collapseOpen);
+  };
+  const onCollapseExiting = () => {
+    setCollapseOut("collapsing-out");
+  };
+  const onCollapseExited = () => {
+    setCollapseOut("");
+  };
+
+  return (
+    <Navbar className={"fixed-top " + color} color-on-scroll="100" expand="lg">
+      <Container>
+        <div className="navbar-translate">
+          
+          <NavbarBrand to="/" tag={Link} id="navbar-brand">
+            <span> Graffam • Tapia </span>
+            - Project HR
+          </NavbarBrand>
+          <UncontrolledTooltip placement="bottom" target="navbar-brand">
+            Frontend - Colton <br/> Backend - Andy
+          </UncontrolledTooltip>
+          <button
+            aria-expanded={collapseOpen}
+            className="navbar-toggler navbar-toggler"
+            onClick={toggleCollapse}
+          >
+            <span className="navbar-toggler-bar bar1" />
+            <span className="navbar-toggler-bar bar2" />
+            <span className="navbar-toggler-bar bar3" />
+          </button>
+        </div>
+        <Collapse
+          className={"justify-content-end " + collapseOut}
+          navbar
+          isOpen={collapseOpen}
+          onExiting={onCollapseExiting}
+          onExited={onCollapseExited}
+        >
+          <div className="navbar-collapse-header">
+            <Row>
+              <Col className="collapse-brand" xs="6">
+                <a href="#rex" onClick={(e) => e.preventDefault()}>
+                  PROJECT•HR
+                </a>
+              </Col>
+              <Col className="collapse-close text-right" xs="6">
+                <button
+                  aria-expanded={collapseOpen}
+                  className="navbar-toggler"
+                  onClick={toggleCollapse}
+                >
+                  <i className="tim-icons icon-simple-remove" />
+                </button>
+              </Col>
+            </Row>
+          </div>
+          <Nav className="ml-auto" navbar>
+          <NavItem>
+              <NavLink href="/Index.js" >
+                <i className="tim-icons icon-single-02" />
+                Profile
+              </NavLink>
+            </NavItem>
+            <NavItem className="active">
+              <NavLink to="/about" tag={Link}>
+                <i className="tim-icons icon-world" />
+                About
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#rex" >
+                <i className="tim-icons icon-settings-gear-63" />
+                Settings
+              </NavLink>
+              <NavLink
+                data-placement="bottom"
+                href="https://www.instagram.com/coltongraffam"
+                rel="noopener noreferrer"
+                target="_blank"
+                title="Follow us on Instagram"
+              >
+                <i className="fab fa-instagram" />
+                <p className="d-lg-none d-xl-none">Instagram</p>
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+
