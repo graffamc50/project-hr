6c66492d1d6f1085fc3250a32fd75bbc022a256b
Colton Bradley Graffam <colton.b.graffam@gmail.com>
2023-09-08 09:00:50 -0700

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,14 +9,12 @@ import { Link } from "react-router-dom";
 import GTicon from "../../assets/img/GTicon.png";
 // reactstrap components
 import {
-  Button,
   NavItem,
   NavLink,
   Nav,
   Container,
   Row,
   Col,
-  UncontrolledTooltip,
 } from "reactstrap";
 
 
@@ -25,8 +23,11 @@ export default function Footer() {
     <footer className="footer">
       <Container>
         <Row>
-          <Col md="3">
+          <Col md="5">
             <h1 className="title">PROJ•HR</h1>
+          </Col>
+          <Col md="5">
+            <img src={GTicon} alt="GTicon" style={{ width: '150px', height: '150px', marginRight: '400px'}} />
           </Col>
           <Col md="2">
             <Nav>
@@ -40,51 +41,12 @@ export default function Footer() {
                   About
                 </NavLink>
               </NavItem>
+              <NavItem>
+                <NavLink to="Index#signUp" tag={Link}>
+                  Sign Up
+                </NavLink>
+              </NavItem>
             </Nav>
-          </Col>
-          <Col md="3">
-            <img src={GTicon} alt="GTicon" style={{ width: '150px', height: '150px', marginRight: '400px'}} />
-          </Col>
-          <Col md="3">
-            <h3 className="title">Follow us:</h3>
-            <div className="btn-wrapper profile">
-              <Button
-                className="btn-icon btn-neutral btn-round btn-simple"
-                color="default"
-                href="https://twitter.com/creativetim"
-                id="tooltip622135962"
-                target="_blank"
-              >
-                <i className="fab fa-twitter" />
-              </Button>
-              <UncontrolledTooltip delay={0} target="tooltip622135962">
-                Follow us
-              </UncontrolledTooltip>
-              <Button
-                className="btn-icon btn-neutral btn-round btn-simple"
-                color="default"
-                href="https://www.facebook.com/creativetim"
-                id="tooltip230450801"
-                target="_blank"
-              >
-                <i className="fab fa-facebook-square" />
-              </Button>
-              <UncontrolledTooltip delay={0} target="tooltip230450801">
-                Like us
-              </UncontrolledTooltip>
-              <Button
-                className="btn-icon btn-neutral btn-round btn-simple"
-                color="default"
-                href="https://dribbble.com/creativetim"
-                id="tooltip318450378"
-                target="_blank"
-              >
-                <i className="fab fa-dribbble" />
-              </Button>
-              <UncontrolledTooltip delay={0} target="tooltip318450378">
-                Follow us
-              </UncontrolledTooltip>
-            </div>
           </Col>
         </Row>
       </Container>
