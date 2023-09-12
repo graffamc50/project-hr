f10e9cdfe7f492d03bf02e49631b4aecf76861a3
Colton Bradley Graffam <colton.b.graffam@gmail.com>
2023-09-12 10:21:00 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 //When needed for compiler and node module errors
+// such as LOG from ./node_modules/sass-loader/dist/cjs.js sass-loader ./node_modules/css-loader/dist/cjs.js??ruleSet[1].rules[1].oneOf[7].use[1]!./node_modules/postcss-loader/dist/cjs.js??ruleSet[1].rules[1].oneOf[7].use[2]!./node_modules/resolve-url-loader/index.js??ruleSet[1].rules[1].oneOf[7].use[3]!./node_modules/sass-loader/dist/cjs.js??ruleSet[1].rules[1].oneOf[7].use[4]!./src/assets/scss/project-hr.scss 
 //npm rebuild node-sass
 
 import React from 'react';
