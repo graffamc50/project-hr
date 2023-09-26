0f3901bc363c8986e3078ef01b964111819f6067
Colton Bradley Graffam <colton.b.graffam@gmail.com>
2023-09-25 20:40:02 -0700

    hopefully this works

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 
-import Index from './views/Index';
+//import Index from './views/Index';
 import AboutPage from './views/Pages/AboutPage';
 import './assets/scss/project-hr.scss';
 import './assets/css/nucleo-icons.css';
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/components" element={<Index />} />
+      <Route path="/components" element={<Navigate to="https://graffamc50.github.io/project-hr/" replace />} />
       <Route path="/about" element={<AboutPage />} />
       <Route path="*" element={<Navigate to="https://graffamc50.github.io/project-hr/" replace />} />
     </Routes>
