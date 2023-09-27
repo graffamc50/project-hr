089144ba9e204cf6ccb65eca5fe25709a0b98a2a
Colton Bradley Graffam <colton.b.graffam@gmail.com>
2023-09-27 13:06:10 -0700

    index Hashrouter change

--- src/index.js
+++ src/index.js
@@ -6,22 +6,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Index from "./views/Index.js";
 import AboutPage from './views/Pages/AboutPage.js';
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom"; // Import HashRouter
 import "./assets/scss/project-hr.scss"
 import "./assets/css/nucleo-icons.css";
 import "./assets/demo/demo.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<Index />} />
       <Route path="/about" element={<AboutPage />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
-
-
