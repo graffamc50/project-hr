52f05fd9677a5ca2752f7625218ac3391116844b
Colton Bradley Graffam <colton.b.graffam@gmail.com>
2023-09-28 00:02:34 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-//When needed for compiler and node module errors
-// such as LOG from ./node_modules/sass-loader/dist/cjs.js sass-loader ./node_modules/css-loader/dist/cjs.js??ruleSet[1].rules[1].oneOf[7].use[1]!./node_modules/postcss-loader/dist/cjs.js??ruleSet[1].rules[1].oneOf[7].use[2]!./node_modules/resolve-url-loader/index.js??ruleSet[1].rules[1].oneOf[7].use[3]!./node_modules/sass-loader/dist/cjs.js??ruleSet[1].rules[1].oneOf[7].use[4]!./src/assets/scss/project-hr.scss 
-//npm rebuild node-sass
+
+
+
+
+
+
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Index from "./views/Index.js";
-import AboutPage from './views/Pages/AboutPage.js';
+import Index from "./landing/views/Index";
+import AboutPage from './landing/views/Pages/AboutPage.js';
+import DashIndex from './dashboard/dashIndex';
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom"; // Import HashRouter
-import "./assets/scss/project-hr.scss"
+import "./assets/scss/project-hr.scss";
 import "./assets/css/nucleo-icons.css";
 import "./assets/demo/demo.css";
 
@@ -18,6 +22,7 @@ root.render(
     <Routes>
       <Route path="/" element={<Index />} />
       <Route path="/about" element={<AboutPage />} />
+      <Route path="/dashboard" element={<DashIndex />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   </HashRouter>
